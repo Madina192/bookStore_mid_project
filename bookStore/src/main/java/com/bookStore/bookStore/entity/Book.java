5fa7c20c7b04b37c62f85d021502e299d6209ad3
Madina192 <madinaakpaeva000@gmail.com>
2023-03-23 21:50:25 +0600

    added bookList and Register pages

--- bookStore/src/main/java/com/bookStore/bookStore/entity/Book.java
+++ bookStore/src/main/java/com/bookStore/bookStore/entity/Book.java
@@ -1,4 +1,60 @@
 package com.bookStore.bookStore.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String author;
+    private String price;
+
+    public Book(int id, String name, String author, String price) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.price = price;
+    }
+
+    public Book() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
 }
